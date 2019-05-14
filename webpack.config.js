9bcbd8deafabd9ab4ccb43e0993eb10bfaa72447
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-14 14:40:37 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const JsConfigWebpackPlugin = require("js-config-webpack-plugin");
+const TsConfigWebpackPlugin = require("ts-config-webpack-plugin");
+const ScssConfigWebpackPlugin = require("scss-config-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: "elm-webpack-loader",
+          options: {}
+        }
+      }
+    ]
+  },
+  plugins: [
+    // Cleans the dist folder before the build starts
+    new CleanWebpackPlugin(),
+    // Generate a base html file and injects all generated css and js files
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    // Multi threading babel loader configuration with caching for .js and .jsx files
+    // see https://github.com/namics/webpack-config-plugins/tree/master/packages/js-config-webpack-plugin/config
+    // new JsConfigWebpackPlugin(),
+    new TsConfigWebpackPlugin(),
+    // SCSS Configuration for .css .module.css and .scss .module.scss files
+    // see https://github.com/namics/webpack-config-plugins/tree/master/packages/scss-config-webpack-plugin/config
+    new ScssConfigWebpackPlugin()
+  ]
+};
