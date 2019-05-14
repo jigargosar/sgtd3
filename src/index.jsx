65e29b7528e4658400ff759336f6b5d7eeb5ff30
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-14 18:17:59 +0530

    sc

--- src/index.jsx
+++ src/index.jsx
@@ -90,6 +90,78 @@ function useActions(setState) {
   }, [setState])
 }
 
+//#region RENDER
+
+function App() {
+  const [state, setState] = useState(loadState)
+  useEffect(() => cacheState(state), [state])
+
+  const actions = useActions(setState)
+
+  const page = R.propOr({ kind: 'MAIN_PAGE' }, 'page')(state)
+
+  if (page.kind === 'MAIN_PAGE') {
+    return renderMainPage(state, actions)
+  } else if (page.kind === 'LINE_DETAIL') {
+    const id = page.id
+    const line = R.find(idEq(id))(state.lines)
+    return (
+      <div>
+        <button onClick={() => actions.onBackClicked()}>Back</button>
+        <div>DETAIL:</div>
+        <div>ID: {line.id}</div>
+        <div>Title: {line.title}</div>
+        <div className="flex">
+          {line.trashed ? (
+            <button onClick={() => actions.lineDetailRestoreClicked(line)}>
+              RESTORE
+            </button>
+          ) : (
+            <button onClick={() => actions.lineDetailDelClicked(line)}>
+              DEL
+            </button>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+function renderMainPage(state, actions) {
+  const trashedLines = R.filter(isTrashed)(state.lines)
+  const trashCt = trashedLines.length
+  const filteredLines = R.reject(isTrashed)(state.lines)
+  const filteredCt = filteredLines.length
+  const renderLines = R.map(line => (
+    <LineLI key={line.id} line={line} actions={actions} />
+  ))
+  const tabNames = ['ALL_TAB', 'TRASH_TAB']
+  const selectedTab = R.propOr('ALL_TAB', 'currentTab')(state)
+  invariant(tabNames.includes(selectedTab))
+  const selTabCN = 'bg-black white'
+  return (
+    <div className="sans-serif">
+      <div className="pa2 flex">
+        <div
+          className={`pa2 ${selectedTab === 'ALL_TAB' ? selTabCN : ''}`}
+          onClick={() => actions.onTabClicked('ALL_TAB')}
+        >
+          ALL_TAB: {filteredCt}
+        </div>
+        <div
+          className={`pa2 ${selectedTab === 'TRASH_TAB' ? selTabCN : ''}`}
+          onClick={() => actions.onTabClicked('TRASH_TAB')}
+        >
+          Trashed: {trashCt}
+        </div>
+      </div>
+      <hr />
+      {selectedTab === 'ALL_TAB' && renderLines(filteredLines)}
+      {selectedTab === 'TRASH_TAB' && renderLines(trashedLines)}
+    </div>
+  )
+}
+
 function LineLI({ line, actions }) {
   return (
     <div className="pa2 code f6">
@@ -119,74 +191,6 @@ function LineLI({ line, actions }) {
   )
 }
 
-function App() {
-  const [state, setState] = useState(loadState)
-  useEffect(() => cacheState(state), [state])
-
-  const actions = useActions(setState)
-
-  const page = R.propOr({ kind: 'MAIN_PAGE' }, 'page')(state)
-
-  if (page.kind === 'MAIN_PAGE') {
-    const trashedLines = R.filter(isTrashed)(state.lines)
-    const trashCt = trashedLines.length
-    const filteredLines = R.reject(isTrashed)(state.lines)
-    const filteredCt = filteredLines.length
-    const renderLines = R.map(line => (
-      <LineLI key={line.id} line={line} actions={actions} />
-    ))
-    const tabNames = ['ALL_TAB', 'TRASH_TAB']
-    const selectedTab = R.propOr('ALL_TAB', 'currentTab')(state)
-    invariant(tabNames.includes(selectedTab))
-
-    const selTabCN = 'bg-black white'
-
-    return (
-      <div className="sans-serif">
-        <div className="pa2 flex">
-          <div
-            className={`pa2 ${selectedTab === 'ALL_TAB' ? selTabCN : ''}`}
-            onClick={() => actions.onTabClicked('ALL_TAB')}
-          >
-            ALL_TAB: {filteredCt}
-          </div>
-          <div
-            className={`pa2 ${
-              selectedTab === 'TRASH_TAB' ? selTabCN : ''
-            }`}
-            onClick={() => actions.onTabClicked('TRASH_TAB')}
-          >
-            Trashed: {trashCt}
-          </div>
-        </div>
-        <hr />
-        {selectedTab === 'ALL_TAB' && renderLines(filteredLines)}
-        {selectedTab === 'TRASH_TAB' && renderLines(trashedLines)}
-      </div>
-    )
-  } else if (page.kind === 'LINE_DETAIL') {
-    const id = page.id
-    const line = R.find(idEq(id))(state.lines)
-    return (
-      <div>
-        <button onClick={() => actions.onBackClicked()}>Back</button>
-        <div>DETAIL:</div>
-        <div>ID: {line.id}</div>
-        <div>Title: {line.title}</div>
-        <div className="flex">
-          {line.trashed ? (
-            <button onClick={() => actions.lineDetailRestoreClicked(line)}>
-              RESTORE
-            </button>
-          ) : (
-            <button onClick={() => actions.lineDetailDelClicked(line)}>
-              DEL
-            </button>
-          )}
-        </div>
-      </div>
-    )
-  }
-}
+//#endregion
 
 render(<App />, document.getElementById('root'))
