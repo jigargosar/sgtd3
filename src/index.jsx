f9771470dc4b45d3b9347f541c31bba4115e0082
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-14 15:12:40 +0530

    init

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,23 @@ import 'tachyons'
 import './index.css'
 import React from 'react'
 import { render } from 'react-dom'
+import faker from 'faker'
+import nanoid from 'nanoid'
+
+function createLine(options) {
+  return {
+    id: `I_${nanoid()}`,
+    title: faker.name.lastName(),
+  }
+}
 
 function App() {
+  const line = createLine()
   return (
-    <div>
+    <div className="sans-serif">
       <div>HW</div>
+      <div>{line.id}</div>
+      <div>{line.title}</div>
     </div>
   )
 }
